0d228d5996377064a99ac76654a96e2ad7db1758
arco <18909336686@163.com>
2016-08-23 18:40:40 +0800

    checkout 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,30 +2,30 @@
 <html>
     <head>
         <title>It's cool</title>
-        <link href="/css/index.css" rel="stylesheet" >
+        <link href="/css/index2.css" rel="stylesheet" >
         {{--<script src="/js/jquery.min.js"></script>--}}
         {{--<script src="/js/bootstrap.min.js"></script>--}}
-        <script src="/js/index.js"></script>
+        <script src="/js/index2.js"></script>
         {{--<script  src="{{\Illuminate\Support\Facades\URL::asset('js/index2.js')}} "></script>--}}
     </head>
     <body>
-    {{--<link href='http://fonts.googleapis.com/css?family=Ubuntu:400,300' rel='stylesheet' type='text/css'>--}}
-    {{--<div class="loader">--}}
-        {{--<canvas width="480px" height="480px" id="loader"></canvas>--}}
-        {{--<h1>Loading</h1>--}}
-    {{--</div>--}}
-    <div id="particles-background" class="vertical-centered-box"></div>
-    <div id="particles-foreground" class="vertical-centered-box"></div>
-
-    <div class="vertical-centered-box">
-        <div class="content">
-            <div class="loader-circle"></div>
-            <div class="loader-line-mask">
-                <div class="loader-line"></div>
-            </div>
-            {!! $star !!}
-        </div>
+    <link href='http://fonts.googleapis.com/css?family=Ubuntu:400,300' rel='stylesheet' type='text/css'>
+    <div class="loader">
+        <canvas width="480px" height="480px" id="loader"></canvas>
+        <h1>Loading</h1>
     </div>
+    {{--<div id="particles-background" class="vertical-centered-box"></div>--}}
+    {{--<div id="particles-foreground" class="vertical-centered-box"></div>--}}
+
+    {{--<div class="vertical-centered-box">--}}
+        {{--<div class="content">--}}
+            {{--<div class="loader-circle"></div>--}}
+            {{--<div class="loader-line-mask">--}}
+                {{--<div class="loader-line"></div>--}}
+            {{--</div>--}}
+            {{--{!! $star !!}--}}
+        {{--</div>--}}
+    {{--</div>--}}
 
     </body>
 </html>
